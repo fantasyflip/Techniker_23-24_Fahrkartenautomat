2f3bd321fc4d2b639e140aa8bd96d304dbcc08dc
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-11-08 09:17:58 +0100

    fix(main): price selection

--- main.c
+++ main.c
@@ -3,24 +3,27 @@
 int main()
 {
     int is_child = 1, single = 0, series = 0, month = 1;
-    double price, output, input = 170.33;
+    double price, output, input = 160.23;
     double currencySteps[] = { 500.0, 200.0, 100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 };
 
     if(series){
         if(is_child){
             price = 4.5;
+        } else {
+            price = 6.5;
         }
-        price = 6.5;
     } else if(month) {
         if(is_child){
             price = 25.5;
+        } else {
+            price = 30.0;
         }
-        price = 30.0;
     } else if (single){
         if(is_child){
             price = 2.5;
+        } else {
+            price = 3.5;
         }
-        price = 3.5;
     }
 
     output = input - price;
@@ -31,22 +34,25 @@ int main()
         if(series){
             if(is_child){
                 printf("Sie erhalten eine Streifenkarte fuer Kinder. Vielen Dank fuer Ihren Einkauf!\n");
+            } else {
+                printf("Sie erhalten eine Streifenkarte fuer Erwachsene. Vielen Dank fuer Ihren Einkauf!\n");
             }
-            printf("Sie erhalten eine Streifenkarte fuer Erwachsene. Vielen Dank fuer Ihren Einkauf!\n");
         } else if(month) {
             if(is_child){
                 printf("Sie erhalten eine Monatskarte fuer Kinder. Vielen Dank fuer Ihren Einkauf!\n");
+            } else {
+                printf("Sie erhalten eine Monatskarte fuer Erwachsene. Vielen Dank fuer Ihren Einkauf!\n");
             }
-            printf("Sie erhalten eine Monatskarte fuer Erwachsene. Vielen Dank fuer Ihren Einkauf!\n");
         } else if (single){
             if(is_child){
                 printf("Sie erhalten eine Einzelkarte fuer Kinder. Vielen Dank fuer Ihren Einkauf!\n");
+            } else {
+                printf("Sie erhalten eine Einzelkarte fuer Erwachsene. Vielen Dank fuer Ihren Einkauf!\n");
             }
-            printf("Sie erhalten eine Einzelkarte fuer Erwachsene. Vielen Dank fuer Ihren Einkauf!\n");
         }
 
         if(output != 0.0){
-            printf("Ihr Rueckgeld betraegt %.2lf und wird in folgender Konstellation ausgegeben:\n", output);
+            printf("Ihr Rueckgeld betraegt %.2lf Euro und wird in folgender Konstellation ausgegeben:\n\n", output);
 
             int currencyItemsAmount = sizeof(currencySteps)/sizeof(currencySteps[0]);
 
